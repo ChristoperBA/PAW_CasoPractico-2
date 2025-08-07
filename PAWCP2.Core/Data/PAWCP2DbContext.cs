6dde9036c457f92ae3b6b37cbffb2ddfa7a97162
AndresALPD <andresalpizar03@gmail.com>
2025-08-06 19:36:20 -0600

    Solucion con scafollding listo

---
+++ PAWCP2.Core/Data/PAWCP2DbContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PAWCP2.Models;
+
+namespace PAWCP2.Data;
+
+public partial class PAWCP2DbContext : DbContext
+{
+    public PAWCP2DbContext()
+    {
+    }
+
+    public PAWCP2DbContext(DbContextOptions<PAWCP2DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FoodItem> FoodItems { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=SQL1004.site4now.net;Database=db_ab9ba9_pawcp2;User Id=db_ab9ba9_pawcp2_admin;Password=Cafecafe01;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FoodItem>(entity =>
+        {
+            entity.HasKey(e => e.FoodItemID).HasName("PK__FoodItem__464DCBF21B0B2D89");
+
+            entity.HasIndex(e => e.Barcode, "UQ__FoodItem__177800D30DADB4A0").IsUnique();
+
+            entity.Property(e => e.Barcode).HasMaxLength(50);
+            entity.Property(e => e.Brand).HasMaxLength(50);
+            entity.Property(e => e.Category).HasMaxLength(50);
+            entity.Property(e => e.DateAdded)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Ingredients).HasMaxLength(500);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.IsPerishable).HasDefaultValue(false);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.QuantityInStock).HasDefaultValue(0);
+            entity.Property(e => e.Supplier).HasMaxLength(100);
+            entity.Property(e => e.Unit).HasMaxLength(20);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.FoodItems)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK__FoodItems__RoleI__46E78A0C");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__Roles__8AFACE1A89375092");
+
+            entity.HasIndex(e => e.RoleName, "UQ__Roles__8A2B6160FA9EA349").IsUnique();
+
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.RoleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4CBA4BF0D9");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E4048C42B7").IsUnique();
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534BB8D7C56").IsUnique();
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.LastLogin).HasColumnType("datetime");
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.RoleId }).HasName("PK__UserRole__AF2760AD5771DFDB");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(10)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Role).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__UserRoles__RoleI__47DBAE45");
+
+            entity.HasOne(d => d.User).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__UserRoles__UserI__48CFD27E");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
